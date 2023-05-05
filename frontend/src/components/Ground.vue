a3b78dd4a90229d6902647781f62794c10bf4222
jeonga-Son <jjeong0106@gmail.com>
2023-05-06 01:17:15 +0900

    add Footer

--- frontend/src/components/Ground.vue
+++ frontend/src/components/Ground.vue
@@ -2,13 +2,15 @@
   <div class="ground">
     <Header />
     <div>Content</div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./Header.vue";
+import Footer from "./Footer.vue";
 export default {
-  components: { Header },
+  components: { Header, Footer },
 };
 </script>
 
