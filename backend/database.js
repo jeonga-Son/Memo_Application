3f0d45e776b6e6cec946b183543fae82504900ee
jeonga-Son <jjeong0106@gmail.com>
2023-05-06 21:50:35 +0900

    update database.js : add 'conn.end()' code

--- backend/database.js
+++ backend/database.js
@@ -9,7 +9,7 @@ const pool = mariadb.createPool({
 
 module.exports = {
   async run(query, params) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       pool
         .getConnection()
         .then((conn) => {
@@ -17,14 +17,18 @@ module.exports = {
             .query(query, params)
             .then((rows) => {
               resolve(rows);
+              conn.end(); // (필수) connection 종료
             })
             .catch((err) => {
               console.log(err);
-              conn.end();
+              conn.end(); // (필수) connection 종료
+              reject(err);
             });
         })
         .catch((err) => {
           //not connected
+          console.log(err);
+          reject(err);
         });
     });
   },
