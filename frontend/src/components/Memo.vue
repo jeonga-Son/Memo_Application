48cca193eda7b1ef7368c7ffcdb221e1c1a48406
jeonga-Son <jjeong0106@gmail.com>
2023-05-06 12:55:09 +0900

    install sass, sass-loader

--- frontend/src/components/Memo.vue
+++ frontend/src/components/Memo.vue
@@ -1,9 +1,28 @@
 <template>
-  <footer>I am memo</footer>
+  <div class="memo">
+    <ul>
+      <li>메모 1 내용</li>
+      <li>메모 2 내용</li>
+      <li>메모 3 내용</li>
+      <li>메모 4 내용</li>
+      <li>메모 5 내용</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.memo ul {
+  list-style: none;
+  padding: 15px 0;
+  margin: 0;
+
+  li {
+    padding: 5px 10px;
+    margin: 5px;
+  }
+}
+</style>
